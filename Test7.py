d795288f184a10c7dd3b88f7982abf83a97f8e5e
Nadi Awad <nadiawad60@hotmail.com>
2017-02-26 14:15:45 +0000

    Added an example for defining and using classes.

---
+++ Test7.py
@@ -0,0 +1,40 @@
+import Test6, fresh_tomatoes
+
+toy_story = Test6.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg",
+                        "https://www.youtube.com/watch?v=Bj4gS1JQzjk")
+
+toy_story2 = Test6.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGJvzx-UP7lJRbGfVmsTnTaN9uM7QASNALtmOYdKu7B5Mi6vUG",
+                        "https://www.youtube.com/watch?v=Bj4gS1JQzjk")
+
+toy_story3 = Test6.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRoUD_y-9sdz1BjkF-Jlm3YjkjzdOK-Wgg1KpNWEgCxwmQZ5ccIWQ",
+                        "https://www.youtube.com/watch?v=Bj4gS1JQzjk")
+
+toy_story4 = Test6.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRve2azVGZsaL9ibhhDUcGvlLSGlvbA9Kk_W1xNFx7Q7Z7Xb8Mp4Q",
+                        "https://www.youtube.com/watch?v=Bj4gS1JQzjk")
+
+toy_story5 = Test6.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg",
+                        "https://www.youtube.com/watch?v=Bj4gS1JQzjk")
+
+toy_story6 = Test6.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "http://www.impawards.com/1995/posters/toy_story_ver1_xlg.jpg",
+                        "https://www.youtube.com/watch?v=Bj4gS1JQzjk")
+
+#print toy_story.storyline
+#toy_story.show_trailer()
+
+
+movies = [toy_story, toy_story2, toy_story3, toy_story4, toy_story5, toy_story6]
+#fresh_tomatoes.open_movies_page(movies)
+#print Test6.Movie.VALID_RATINGS
+#print Test6.Movie.__doc__
